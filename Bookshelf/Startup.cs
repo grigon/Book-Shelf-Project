cb5d672a868dbb708a13db605588a7e5f310c6ea
martaskarbek <marta@skarbek.name>
2021-02-02 11:11:41 +0100

    new solution

--- Bookshelf/Startup.cs
+++ Bookshelf/Startup.cs
@@ -10,8 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace BookShelfProject
+namespace Bookshelf
 {
     public class Startup
     {
@@ -26,6 +27,7 @@ namespace BookShelfProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Bookshelf", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,8 @@ namespace BookShelfProject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bookshelf v1"));
             }
 
             app.UseHttpsRedirection();
