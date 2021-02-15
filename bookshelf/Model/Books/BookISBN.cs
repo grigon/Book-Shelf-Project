fd6c0b5a5b3baf607a37c7cd2596e40be6807bb6
martaskarbek <marta@skarbek.name>
2021-02-15 15:59:35 +0100

    added new models

---
+++ bookshelf/Model/Books/BookISBN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace bookshelf.Model.Books
+{
+    public class BookISBN
+    {
+        public Guid Id { get; set; }
+        public Book Book { get; set; }
+        public string ISBNLight { get; set; }
+        public string ISBNHard { get; set; }
+    }
+}
