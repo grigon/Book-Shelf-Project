426205ab4d82657057e6b0e91b5622513af220ba
Dominik Wikliński <domel2222@gmail.com>
2021-03-27 13:40:25 +0100

    cretared Globar Errorhandling Middleware

---
+++ bookshelf/Middlewere/ErrorHandligMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using bookshelf.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookshelf.Middlewere
+{
+    public class ErrorHandligMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandligMiddleware> _logger;
+
+        public ErrorHandligMiddleware(ILogger<ErrorHandligMiddleware> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequest)
+            {
+                _logger.LogError(badRequest.Message);
+                context.Response.StatusCode = 400;
+
+                await context.Response.WriteAsync(badRequest.Message);
+                //await context.Response.CompleteAsync();
+            }
+        }
+    }
+}
